dc1a521efe125bddfd64b285c6d8efdaad82263a
DxeiZ <79997967+DxeiZ@users.noreply.github.com>
2024-03-30 19:14:45 +0400

    Update main.py

--- main.py
+++ main.py
@@ -3,14 +3,20 @@
 import shutil
 from urllib import request
 import colorama
+import signal
 import os
 import json
 import re
 
-os.system("clear")
+def clear_screen():
+  if os.name == 'nt':
+    os.system('cls')
+  else:
+    os.system('clear')
+
+clear_screen()
 
 colorama.init()
-
 
 def Header():
     print(colorama.Fore.RED+"""
@@ -23,16 +29,27 @@ $$$$$$\ $$$$$$$\   $$$$$$\
   $$ |  $$ |      $$\   $$ |$$ |     $$  __$$ |$$ |  $$ |
 $$$$$$\ $$ |      \$$$$$$  |\$$$$$$$\\$$$$$$$ |$$ |  $$ |
 \______|\__|       \______/  \_______|\_______|\__|  \__|
-                                    \033[1;39mDeveloper by Eratonos\033[1;31m
+                                        \033[1;39mDeveloper by DxeiZ\033[1;31m
 """.center(shutil.get_terminal_size().columns))
-
 
 Header()
 
+def human_format(num):
+    magnitude = 0
+    while abs(num) >= 1000:
+        magnitude += 1
+        num /= 1000.0
+    return '%.2f%s' % (num, ['', ' Bin', ' Milyon', ' Milyar', ' Trilyon', 'Katrilyon'][magnitude])
+
+def exit_code(sig, frame):
+  print("\n"+colorama.Fore.LIGHTRED_EX + "Uygulama Kapatılıyor...")
+  exit()
+
+signal.signal(signal.SIGINT, exit_code)
+
 while True:
     print(colorama.Style.RESET_ALL)
-    print(colorama.Fore.GREEN + "[+] " + colorama.Fore.LIGHTRED_EX + "İP Adresini Daxil Edin ↓")
-    ip = input(colorama.Style.RESET_ALL+"> ")
+    ip = input(colorama.Fore.GREEN + "[+] " + colorama.Fore.LIGHTRED_EX + "IP Adresini Girin "+colorama.Style.RESET_ALL+"> ")
 
     regex = "^((25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])\.){3}(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])$"
     p = re.compile(regex)
@@ -44,28 +61,25 @@ while True:
         result = json.loads(response.read().decode("utf-8"))
 
         if len(result) == 5:
-            print(colorama.Fore.RED+f"\n\033[1;3mError: {str(result['reason'])}\033[0m\n"+
+            print(colorama.Fore.RED+f"\n\033[1;3mHATA: {str(result['reason'])}\033[0m\n"+
                 colorama.Fore.LIGHTYELLOW_EX+f"\033[1;3m· IP · {colorama.Fore.LIGHTGREEN_EX+str(result['ip'])}\n"+
-                colorama.Fore.LIGHTYELLOW_EX+f"\033[1;3m· Type · {colorama.Fore.LIGHTGREEN_EX+str(result['version'])}")
+                colorama.Fore.LIGHTYELLOW_EX+f"\033[1;3m· Tür · {colorama.Fore.LIGHTGREEN_EX+str(result['version'])}")
 
         else:
-            print(colorama.Fore.LIGHTYELLOW_EX+f"\033[1;3m· IP · {colorama.Fore.LIGHTGREEN_EX+str(result['ip'])}\n"+
-                  colorama.Fore.LIGHTYELLOW_EX+f"\033[1;3m· Network · {colorama.Fore.LIGHTGREEN_EX+str(result['network'])}\n"+
-                  colorama.Fore.LIGHTYELLOW_EX+f"\033[1;3m· Type · {colorama.Fore.LIGHTGREEN_EX+str(result['version'])}\n"+
-                  colorama.Fore.LIGHTYELLOW_EX+f"\033[1;3m· Country · {colorama.Fore.LIGHTGREEN_EX+str(result['country_name'])}\n"+
-                  colorama.Fore.LIGHTYELLOW_EX+f"\033[1;3m· Country Code · {colorama.Fore.LIGHTGREEN_EX+str(result['country'])}\n"+
-                  colorama.Fore.LIGHTYELLOW_EX+f"\033[1;3m· Country Phone · {colorama.Fore.LIGHTGREEN_EX+str(result['country_calling_code'])}\n"+
-                  colorama.Fore.LIGHTYELLOW_EX+f"\033[1;3m· Country Tld · {colorama.Fore.LIGHTGREEN_EX+str(result['country_tld'])}\n"+
-                  colorama.Fore.LIGHTYELLOW_EX+f"\033[1;3m· Region · {colorama.Fore.LIGHTGREEN_EX+str(result['region'])}\n"+
-                  colorama.Fore.LIGHTYELLOW_EX+f"\033[1;3m· City · {colorama.Fore.LIGHTGREEN_EX+str(result['city'])}\n"+
-                  colorama.Fore.LIGHTYELLOW_EX+f"\033[1;3m· Asn · {colorama.Fore.LIGHTGREEN_EX+str(result['asn'])}\n"+
-                  colorama.Fore.LIGHTYELLOW_EX+f"\033[1;3m· Org · {colorama.Fore.LIGHTGREEN_EX+str(result['org'])}\n"+
-                  colorama.Fore.LIGHTYELLOW_EX+f"\033[1;3m· Timezone · {colorama.Fore.LIGHTGREEN_EX+str(result['timezone'])} ({colorama.Fore.LIGHTGREEN_EX+str(result['utc_offset'])})\n"+
-                  colorama.Fore.LIGHTYELLOW_EX+f"\033[1;3m· Currency ·  {colorama.Fore.LIGHTGREEN_EX+str(result['currency_name'])}\n"+
-                  colorama.Fore.LIGHTYELLOW_EX+f"\033[1;3m· Country Population · {colorama.Fore.LIGHTGREEN_EX+str(result['country_population'])}\n"+
-                  colorama.Fore.LIGHTYELLOW_EX+f"\033[1;3m· Languages · {colorama.Fore.LIGHTGREEN_EX+str(result['languages'])}\n"+
-                  colorama.Fore.LIGHTYELLOW_EX+f"\033[1;3m· Postal · {colorama.Fore.LIGHTGREEN_EX+str(result['postal'])}\n"+
-                  colorama.Fore.LIGHTYELLOW_EX+f"\033[1;3m· Location · {colorama.Fore.LIGHTGREEN_EX+'https://google.com/maps/place/'+str(result['latitude'])},{str(result['longitude'])} ({str(result['latitude'])},{str(result['longitude'])})")
+            populatioNum = human_format(result['country_population'])
+            countyDts = f"{str(result['country_name'])}, {str(result['region'])}, {str(result['city'])} ({str(result['country'])})"
+            network = f"{str(result['ip'])} ({str(result['network'])})"
+            print(colorama.Fore.LIGHTYELLOW_EX+f"\033[1;3m⤚IP⤍  {colorama.Fore.LIGHTGREEN_EX+network}\n"+
+                  colorama.Fore.LIGHTYELLOW_EX+f"\033[1;3m⤚Ülke⤍  {colorama.Fore.LIGHTGREEN_EX+countyDts}\n"+
+                  colorama.Fore.LIGHTYELLOW_EX+f"\033[1;3m    ⤿ Nüfus · {colorama.Fore.LIGHTGREEN_EX+populatioNum}\n"+
+                  colorama.Fore.LIGHTYELLOW_EX+f"\033[1;3m    ⤿ Telefon · {colorama.Fore.LIGHTGREEN_EX+str(result['country_calling_code'])}\n"+
+                  colorama.Fore.LIGHTYELLOW_EX+f"\033[1;3m    ⤿ Tld · {colorama.Fore.LIGHTGREEN_EX+str(result['country_tld'])}\n"+
+                  colorama.Fore.LIGHTYELLOW_EX+f"\033[1;3m    ⤿ Asn · {colorama.Fore.LIGHTGREEN_EX+str(result['asn'])}\n"+
+                  colorama.Fore.LIGHTYELLOW_EX+f"\033[1;3m    ⤿ Org · {colorama.Fore.LIGHTGREEN_EX+str(result['org'])}\n"+
+                  colorama.Fore.LIGHTYELLOW_EX+f"\033[1;3m⤚Zaman Dilimi⤍  {colorama.Fore.LIGHTGREEN_EX+str(result['timezone'])} ({colorama.Fore.LIGHTGREEN_EX+str(result['utc_offset'])})\n"+
+                  colorama.Fore.LIGHTYELLOW_EX+f"\033[1;3m⤚Para Birimi⤍  {colorama.Fore.LIGHTGREEN_EX+str(result['currency_name'])}\n"+
+                  colorama.Fore.LIGHTYELLOW_EX+f"\033[1;3m⤚Posta⤍  {colorama.Fore.LIGHTGREEN_EX+str(result['postal'])}\n"+
+                  colorama.Fore.LIGHTYELLOW_EX+f"\033[1;3m⤚Konum⤍  {colorama.Fore.LIGHTGREEN_EX+'https://google.com/maps/place/'+str(result['latitude'])},{str(result['longitude'])} ({str(result['latitude'])},{str(result['longitude'])})")
 
     else:
-        print(colorama.Fore.GREEN + "[-] " + colorama.Fore.RED + "İP adresi səhvdir!")
+        print(colorama.Fore.GREEN + "[-] " + colorama.Fore.RED + "IP Adresi Yanlış!")
